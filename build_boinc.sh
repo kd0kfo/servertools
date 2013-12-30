ddc476d0b0879f8e592320b70fc6907faecb1bd4
David Coss <David.Coss@stjude.org>
2013-12-30 11:40:00 -0600

    Updated boinc build script.

--- build_boinc.sh
+++ build_boinc.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-BOINC_TARGET_DIR=$1
+set -e
+if [[ -z $1 ]];then
+	echo "Missing boinc source directory." >&2
+	exit 1
+fi
 
-./configure --enable-libraries --disable-client --enable-server --enable-install-headers --prefix=${BOINC_TARGET_DIR} --enable-shared
+cd $1
 
+BOINC_TARGET_DIR=$PWD
+./_autosetup
+./configure --enable-libraries --disable-client --enable-server --enable-install-headers --prefix=${BOINC_TARGET_DIR}/INSTALLDIR --enable-shared
+
+make
+
+make install
